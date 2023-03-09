52ed0ed06bb924a48aaf06878f554e4091874ad3
JFEstevezC <jose2224646@correo.uis.edu.co>
2023-03-09 10:27:40 -0500

    Actualización 2

--- orden_num.py
+++ orden_num.py
@@ -6,19 +6,16 @@ a=int(input("Escriba el primer número: "))
 b=int(input("Escriba el segundo número: "))
 c=int(input("Escriba el tercer número: "))
 
-if a > b:
-    if a > c:
-        if b > c:
-            r = c, b, a
-        else:
-            r = b, c, a
-    else:
-        r = b, a, c
-elif b > c:
-    if c > a:
-        r = a, c, b
-    else:
-        r = c, a, b
+if a > b > c:
+    r = c, b, a
+elif a > c > b:
+    r = b, c, a
+elif c > a > b:
+    r = b, a, c
+elif b > c > a:
+    r = a, c, b
+elif b > a > c:
+    r = c, a, b
 else:
     r = a, b, c
 print("Los números en orden ascendente es")
